bc5e35db59b7b8abe4aec037469b5456bfc2cf3f
shilaj <shilajbaral@gmail.com>
2024-11-19 23:23:54 +0900

    Finished: simple python code

--- advection.py
+++ advection.py
@@ -1,18 +1,29 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import imageio
+from copy import deepcopy
 
 # CONSTANTS: Initial setup parameters:
-L = 1 # Length of the domain
-n_x = 101 # Number of grid points
-c = 0.01 # Wave speed
-t_initial = 0 # Initial time
-t_final = 1 # Final time
-dt = 0.1 # Time step size
+L:int = 1 # Length of the domain
+n_x:int = 101 # Number of grid points
+c:float = 0.01 # Wave speed
+t_initial:float = 0.0 # Initial time
+t_final:float = 1.0 # Final time
+dt:float = 0.1 # Time step size
 
 x, dx = np.linspace(0, L, n_x, retstep=True) # Grid points and grid spacing
 
-def initial_condition(x):
+def initial_condition(x:np.ndarray) -> np.ndarray:
+    '''
+    This function sets the initial condition.
+    The initial condition is a step function between 0.1 and 0.3.
+
+    Parameters:
+    x: The grid points
+
+    Returns:
+    u_x_0: The velocity field at time t = 0
+    '''
     u_x_0 = np.zeros_like(x) # Setting all the values in the velocity field to zero
 
     # Looping over all the grid points and setting the velocity field to 1 between 0.1 and 0.3
@@ -21,7 +32,26 @@ def initial_condition(x):
             u_x_0[i] = 1
     return u_x_0
 
-def analytical_solution(x, t):
+def analytical_solution(x:np.ndarray, t:float) -> np.ndarray:
+    '''
+    This function calculates the analytical solution of the advection equation at time t.
+    
+    Parameters:
+    x: The grid points
+    t: The time at which the solution is calculated
+    
+    Returns:
+    u_x_t: The velocity field at time t
+
+    Functionality:
+    The analytical solution of the advection equation is given by:
+    c = velocity of the wave
+    c * t = distance travelled by the wave
+
+    if distance traveled by the wave is in the domain [0.1, 0.3]
+    at a spatial location x, then we can say that the wave is at that location. 
+    Hence, set the velocity field to 1 at that location.
+    '''
     u_x_t = np.zeros_like(x) # Setting all the values in the velocity field to zero
 
     # Looping over all the grid points and setting the velocity field to 1 between 0.1 and 0.3
@@ -30,33 +60,67 @@ def analytical_solution(x, t):
             u_x_t[i] = 1
     return u_x_t
 
-def numerical_solution(u_old):
+def numerical_solution(u_old:np.ndarray) -> np.ndarray:
+    '''
+    This function calculates the numerical solution of the advection equation using the backward difference scheme.
+
+    Parameters:
+    u_old: The velocity fields at the previous time step
+
+    Returns:
+    u_new: The velocity fields at the current time step
+
+    Functionality:
+    The backward difference scheme is given by:
+    du/dt + c * du/dx = 0
+    '''
     u_new = np.zeros_like(u_old)
     for i in range(1, n_x-1):
         u_new[i] = u_old[i] - c * dt / dx * (u_old[i] - u_old[i-1]) # Using the backward difference scheme
     return u_new
 
-def animate_solution(func, x, type='analytical'):
+def animate_combined_solutions(x: np.ndarray) -> None:
+    '''
+    Create a single GIF combining initial condition, analytical solution,
+    and numerical solution with different colors.
+
+    Parameters:
+    x: The grid points
+
+    Returns:
+    None
+    '''
+    # Initialize solutions
+    u_initial = initial_condition(x)  # Initial condition
+    u_numerical = deepcopy(u_initial)  # Temporary variable for numerical solution
+    
+    # Lists to store frames
     images_list = []
-    time_list = np.arange(t_initial, t_final+dt, dt).round(1)
+    time_list = np.arange(t_initial, t_final + dt, dt).round(1)
+    
     for t in time_list:
-        images_list.append(plot_solution(func(x, t), x, t))
-    return imageio.mimsave(f'./advection/advection_{type}.gif',images_list , fps=1)
+        # Compute solutions
+        u_analytical = analytical_solution(x, t)
+        u_numerical = numerical_solution(u_numerical)
+        
+        # Plot solutions for the current time step
+        fig, ax = plt.subplots(figsize=(15, 8))
+        ax.plot(x, u_initial, linewidth=3, label='Initial Condition', color='blue')  # Initial condition
+        ax.plot(x, u_analytical, linewidth=3, label='Analytical Solution', color='green')  # Analytical solution
+        ax.plot(x, u_numerical, linewidth=3, label='Numerical Solution', color='red')  # Numerical solution
+        ax.grid()
+        ax.set(xlabel='x', ylabel=f'u(x, {t})', title=f'Advection Equation Solutions at Time t = {t}')
+        ax.legend()
 
-def plot_solution(variable, x, t, save=False, type='analytical'):
-    fig, ax = plt.subplots(figsize=(15, 8))
-    ax.plot(x, variable)
-    ax.grid()
-    ax.set(xlabel='x', ylabel=f'u(x, {t})', title='Advection equation solution at time t = {}'.format(t))
-
-    fig.canvas.draw()
-    image = np.frombuffer(fig.canvas.tostring_rgb(), dtype='uint8')
-    image = image.reshape(fig.canvas.get_width_height()[::-1] + (3,))
-
-    return image if not save else fig.savefig(f'./advection/advection_{type}_{t}.png')
+        # Convert plot to image
+        fig.canvas.draw()
+        image = np.frombuffer(fig.canvas.tostring_rgb(), dtype='uint8')
+        image = image.reshape(fig.canvas.get_width_height()[::-1] + (3,))
+        images_list.append(image)
+        plt.close(fig)  # Close the figure to save memory
+    
+    # Save the GIF
+    imageio.mimsave('advection_combined.gif', images_list, fps=1)
 
 if __name__ == '__main__':
-    u_x_0 = initial_condition(x)
-    for t in np.arange(t_initial, t_final, dt).round(1):
-        u_x_t = analytical_solution(x, t)
-        plot_solution(u_x_t, x, t, save=True)
+    animate_combined_solutions(x)
---
+++ diffusion.py
@@ -0,0 +1,90 @@
+import numpy
+import matplotlib.pyplot as plt
+import imageio
+from copy import deepcopy
+
+# CONSTANTS: Initial setup parameters:
+temperature_left: float = 300.0 # Temperature at the left boundary in Kelvin
+temperature_right: float = 273.0 # Temperature at the right boundary in Kelvin
+L: float = 1.0 # Length of the rod in meters
+alpha: int = 1
+dt: float = 0.001
+dx: float = 0.05 
+t_initial: float = 0.0
+t_final: float = 0.05
+
+x = numpy.arange(0, L+dx, dx).round(2) # Grid points and grid spacing
+
+def initial_condition(x:numpy.ndarray) -> numpy.ndarray:
+    temperature_initial = numpy.full_like(x, 350.0)
+    return temperature_initial
+
+def numerical_solution(temperature_old:numpy.ndarray) -> numpy.ndarray:
+    '''
+    Compute the numerical solution using the central difference scheme.
+
+    Parameters:
+    temp_old: The temperature distribution at the previous time step
+
+    Returns:
+    temp_new: The temperature distribution at the current time step
+
+    Equation:
+    dT/dt = alpha * d^2T/dx^2 
+
+    Discretization: (Central difference scheme)
+    t_new = t_old + dt * alpha * (t_old[i+1] - 2*t_old[i] + t_old[i-1]) / dx^2
+    '''
+    temperature_new = deepcopy(temperature_old)
+    for i in range(1, len(temperature_old) - 1):
+        temperature_new[i] = temperature_old[i] + alpha * dt * (temperature_old[i+1] - 2*temperature_old[i] + temperature_old[i-1]) / dx**2
+
+    # Setting Dirichlet boundary conditions:
+    temperature_new[0] = temperature_left
+    temperature_new[-1] = temperature_right
+
+    return temperature_new
+
+def animate_combined_solutions(x: numpy.ndarray) -> None:
+    '''
+    Create a single GIF combining initial condition,
+    and numerical solution with different colors.
+
+    Parameters:
+    x: The grid points
+
+    Returns:
+    None
+    '''
+    # Initialize solutions
+    temperature_initial = initial_condition(x)  # Initial condition
+    temperature_numerical = deepcopy(temperature_initial)  # Temporary variable for numerical solution
+    
+    # Lists to store frames
+    images_list = []
+    time_list = numpy.arange(t_initial, t_final + dt, dt).round(3)
+    
+    for t in time_list:
+        # Compute solutions
+        temperature_numerical = numerical_solution(temperature_numerical)
+        
+        # Plot solutions for the current time step
+        fig, ax = plt.subplots(figsize=(15, 8))
+        ax.plot(x, temperature_initial, linewidth=3, label='Initial Condition', color='blue')  # Initial condition
+        ax.plot(x, temperature_numerical, linewidth=3, label='Numerical Solution', color='red')  # Numerical solution
+        ax.grid()
+        ax.set(xlabel='x', ylabel=f'T(x, {t})', title=f'Diffusion Equation Solutions at Time t = {t}')
+        ax.legend()
+
+        # Convert plot to image
+        fig.canvas.draw()
+        image = numpy.frombuffer(fig.canvas.tostring_rgb(), dtype='uint8')
+        image = image.reshape(fig.canvas.get_width_height()[::-1] + (3,))
+        images_list.append(image)
+        plt.close(fig)  # Close the figure to save memory
+    
+    # Save the GIF
+    imageio.mimsave('diffusion_combined.gif', images_list, fps=5)
+
+if __name__ == '__main__':
+    animate_combined_solutions(x)
